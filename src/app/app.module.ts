f05945c7197c89b5c4af4c4fd0ab026676c7daaa
Gauna Lucas Fabian <166007@coto.com.ar>
2023-02-22 21:29:50 -0300

    Configurando firechat y creando componente chat

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ChatComponent } from './components/chat/chat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp( environment.firebase )
+  ],
+  providers: [AngularFirestore],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+    projectId: 'firechat-32d76',
+    appId: '1:849277595661:web:81be6cb581beff50ab6544',
+    storageBucket: 'firechat-32d76.appspot.com',
+    apiKey: 'AIzaSyCRlLrXAvnJ7pZYw0YOdCE-TNnhXQNiptA',
+    authDomain: 'firechat-32d76.firebaseapp.com',
+    messagingSenderId: '849277595661',
+    measurementId: 'G-KM6HXV8TXV',
+    }
+};
